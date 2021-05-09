f6dad08a0ec0ee91746686572f0d7f997e8e76a6
alastair-buchanan <buchanan.alastair@gmail.com>
2021-05-09 19:01:05 +1000

    Removed functionality from dashboard component for efficiency

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,22 +7,12 @@ import { ReusablePieChart } from "./graphs/ReusablePieChart";
 import { GroupedByChannelPie } from "./graphs/GroupedByChannelPie";
 import { ReusableStackedBar } from "./graphs/ReusableStackedBar";
 import { BarIssuePerOrg } from "./graphs/BarIssuePerOrg";
-
+import { AgeFilter } from "./searchBars/AgeFilter";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
-function countOrganisations(dataSet) {
-  return Object.keys(dataSet).map((element) => ({
-    Organisation: element,
-    Count: dataSet[element].length,
-  }));
-}
-
-function countChannels(dataSet) {
-  return Object.keys(dataSet).map((element) => ({
-    Channel: element,
-    Count: dataSet[element].length,
-  }));
+function filterByAge(data, param) {
+  return data.filter((stock) => stock.Age === param);
 }
 
 export const Dashboard = () => {
@@ -30,8 +20,8 @@ export const Dashboard = () => {
   const [rowData, setRowData] = useState([]);
   const [groupedByOrg, setGroupedByOrg] = useState([]);
   const [groupedByChannel, setGroupedByChannel] = useState([]);
-  const [chartOne, setChartOne] = useState();
-  const [chartTwo, setChartTwo] = useState();
+  const [groupedByIssue, setGroupedByIssue] = useState([]);
+  const [ageFilter, setAgeFilter] = useState();
 
   function groupData(dataSet, property) {
     return dataSet.reduce((acc, obj) => {
@@ -45,33 +35,19 @@ export const Dashboard = () => {
   }
 
   useEffect(() => {
-    setRowData(clientData);
-  }, [clientData]);
-
-  useEffect(() => {
-    setGroupedByOrg(groupData(clientData, "Organisation"));
-    console.log("groupedByOrg data", groupedByOrg);
-    console.log("rowData: ", rowData);
-  }, [rowData, clientData]);
-
-  useEffect(() => {
-    setGroupedByChannel(groupData(clientData, "Channel"));
-    console.log("groupedByChannel data", groupedByChannel);
-  }, [rowData, clientData]);
-
-  useEffect(() => {
-    if (groupedByOrg !== null && groupedByOrg !== undefined) {
-      setChartOne(countOrganisations(groupedByOrg));
+    let data = clientData;
+    if (ageFilter !== null) {
+      data = filterByAge(data, ageFilter);
     }
-    console.log("chartOne data", chartOne);
-  }, [groupedByOrg]);
+    setRowData(data);
+    console.log("rowdata by date", rowData);
+  }, [clientData, ageFilter, rowData.props]);
 
   useEffect(() => {
-    if (groupedByChannel !== null && groupedByChannel !== undefined) {
-      setChartTwo(countChannels(groupedByChannel));
-    }
-    console.log("chartOne data", chartOne);
-  }, [groupedByChannel]);
+    setGroupedByOrg(groupData(rowData, "Organisation"));
+    setGroupedByIssue(groupData(rowData, "Problem_Category"));
+    setGroupedByChannel(groupData(rowData, "Channel"));
+  }, [rowData, clientData]);
 
   if (loading) {
     return <div>loading</div>;
@@ -80,27 +56,41 @@ export const Dashboard = () => {
     return <div>console.error();</div>;
   }
   return (
-    <Grid celled="internally">
+    <Grid celled>
       <Grid.Row>
         <Grid.Column width={4}>
-          <GeneralTable width="10%" rowData={rowData} />
+          <AgeFilter onSubmit={setAgeFilter} />
+        </Grid.Column>
+        <Grid.Column width={4}></Grid.Column>
+        <Grid.Column width={4}></Grid.Column>
+        <Grid.Column width={4}></Grid.Column>
+      </Grid.Row>
+      <Grid.Row width={16}>
+        <Grid.Column width={4}>
+          <GeneralTable
+            width="10%"
+            rowData={rowData}
+            organisations={groupedByOrg}
+            issues={groupedByIssue}
+            channels={groupedByChannel}
+          />
         </Grid.Column>
         <Grid.Column width={6}>
-          <ReusableRechart rowData={chartOne} />
+          <ReusableRechart rowData={groupedByOrg} />
         </Grid.Column>
         <Grid.Column width={6}>
-          <ReusablePieChart rowData={chartOne} />
+          <ReusablePieChart rowData={groupedByOrg} />
         </Grid.Column>
       </Grid.Row>
       <Grid.Row>
         <Grid.Column width={4}>
-          <GroupedByChannelPie rowData={chartTwo} />
+          <GroupedByChannelPie rowData={groupedByChannel} />
         </Grid.Column>
         <Grid.Column width={6}>
-          <ReusableStackedBar clientData={clientData} />
+          <ReusableStackedBar clientData={rowData} />
         </Grid.Column>
         <Grid.Column width={6}>
-          <BarIssuePerOrg clientData={clientData} />
+          <BarIssuePerOrg clientData={rowData} />
         </Grid.Column>
       </Grid.Row>
     </Grid>
--- src/components/tables/GeneralTable.js
+++ src/components/tables/GeneralTable.js
@@ -1,39 +1,48 @@
 import React, { useEffect, useState } from "react";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableRow,
-} from "semantic-ui-react";
+import { Table, TableBody, TableCell, TableRow } from "semantic-ui-react";
 
-export const GeneralTable = ({ rowData }) => {
+// function sortOrganisation(dataSet) {
+//   const newArray = ([]);
+//   return Object.keys(dataSet)
+//     .map((element) => ({
+//       Organisation: element,
+//       Count: dataSet[element].length,
+//     }))
+
+// }
+
+export const GeneralTable = ({ rowData, organisations, issues, channels }) => {
   const [numberClients, setNumberClients] = useState();
+  const [mostOrganisation, setMostOrganisation] = useState();
+  const [mostIssues, setMostIssues] = useState();
+  const [mostChannels, setMostChannels] = useState();
 
-  useEffect(() => {
-    setNumberClients(rowData.length);
-  }, []);
+  // useEffect(() => {
+  //   setNumberClients(rowData.length);
+  //   setMostOrganisation(sortOrganisation(organisations).sort((a, b) => (a.Count > b.Count ? 1 : b.last_nom > a.last_nom ? -1 : 0)));
+  // }, [rowData, organisations, issues, channels]);
 
   return (
     <Table celled>
       <TableBody>
         <TableRow>
-          <TableCell>Number of clients:</TableCell>
+          <TableCell>Number of users:</TableCell>
+          <TableCell>{rowData.length}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Number of organisations:</TableCell>
+          <TableCell>{rowData.length}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Highest Age group:</TableCell>
           <TableCell>{numberClients}</TableCell>
         </TableRow>
         <TableRow>
-          <TableCell>Number of clients:</TableCell>
+          <TableCell>Greatest issue:</TableCell>
           <TableCell>{numberClients}</TableCell>
         </TableRow>
         <TableRow>
-          <TableCell>Number of clients:</TableCell>
-          <TableCell>{numberClients}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>Number of clients:</TableCell>
-          <TableCell>{numberClients}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>Number of clients:</TableCell>
+          <TableCell>Highest volume day:</TableCell>
           <TableCell>{numberClients}</TableCell>
         </TableRow>
         <TableRow>
