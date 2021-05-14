cca4984729c81f779d3494400112fdac9108d0c3
alastair-buchanan <buchanan.alastair@gmail.com>
2021-05-14 14:12:15 +1000

    updated dashboard 2

--- src/components/Dashboard2.js
+++ src/components/Dashboard2.js
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import PivotTableUI from "react-pivottable/PivotTableUI";
 import "react-pivottable/pivottable.css";
 import TableRenderers from "react-pivottable/TableRenderers";
 import Plot from "react-plotly.js";
 import createPlotlyRenderers from "react-pivottable/PlotlyRenderers";
 import { useClientData } from "../api/Api";
-import { Container } from "semantic-ui-react";
 
 const PlotlyRenderers = createPlotlyRenderers(Plot);
 
@@ -13,19 +12,21 @@ export const Dashboard2 = () => {
   const { loading, clientData, error } = useClientData();
   const [state, setState] = useState();
 
+  const componentRef = useRef();
+
+  if (loading) {
+    return <div>loading</div>;
+  }
+  if (error) {
+    return <div>console.error();</div>;
+  }
   return (
-    <Container>
       <PivotTableUI
+        ref={componentRef}
         data={clientData}
         onChange={(s) => setState(s)}
         renderers={Object.assign({}, TableRenderers, PlotlyRenderers)}
         {...state}
       />
-      <span style={{ float: "right" }}>
-        <button onClick={() => exportComponentAsJPEG(componentRef)}>
-          Download
-        </button>
-      </span>
-    </Container>
   );
 };
